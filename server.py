4589bf47edc97d4b254629ee8f87ac9d110ca8f2
nelsonGX <hi@nelsongx.com>
2024-09-05 12:27:26 +0800

    add try/except to minimize error

--- server.py
+++ server.py
@@ -16,23 +16,31 @@ async def check():
 @app.route('/ask', methods=['POST'])
 async def ask():
     print("# [server.py] [ask] Request received")
-    return_messasge = await process_message(await request.form, await request.files, False)
-    
-    if "error" in return_messasge:
+    try:
+        return_messasge = await process_message(await request.form, await request.files, False)
+        
+        if "error" in return_messasge:
+            return jsonify({"error": "ERROR"}), 500
+        return jsonify(return_messasge)
+    except Exception as e:
+        print("# [server.py] [ask] Error: " + str(e))
         return jsonify({"error": "ERROR"}), 500
-    return jsonify(return_messasge)
 
 @app.route('/return', methods=['POST'])
 async def return_value():
     print("# [server.py] [return_value] Request received")
-    if "image" in (await request.files):
-        return_message = await process_message(await request.form, (await request.files)["image"] , True)
-    else:
-        return_message = await process_message(await request.form, None, True)
+    try:
+        if "image" in (await request.files):
+            return_message = await process_message(await request.form, (await request.files)["image"] , True)
+        else:
+            return_message = await process_message(await request.form, None, True)
 
-    if "error" in return_message:
+        if "error" in return_message:
+            return jsonify({"error": "ERROR"}), 500
+        return jsonify(return_message)
+    except Exception as e:
+        print("# [server.py] [return_value] Error: " + str(e))
         return jsonify({"error": "ERROR"}), 500
-    return jsonify(return_message)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
